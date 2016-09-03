6630a4871e93a14b7597fa87856a8a930053a3eb
mmazanec22 <melanie.mazanec@gmail.com>
2016-09-03 16:54:25 -0500

    Shady S3 links

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -10,6 +10,13 @@ class Complaint < ApplicationRecord
 
   accepts_nested_attributes_for :allegations
 
+  def media_urls
+    # signer = Aws::S3::Presigner.new
+    # urls = self.media.map{|obj| signer.presigned_url(obj, bucket: ENV['S3_BUCKET_NAME'], key: ENV['S3_KEY']).to_s}
+    urls = self.media.map{|obj| obj}
+    urls
+  end
+
   def add_allegations
   end
 
