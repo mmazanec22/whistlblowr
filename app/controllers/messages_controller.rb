1d9f758f16c6d5a56c699ed2732593f8a1527aa5
Joseph Mazrimas <email": null>
2016-09-04 14:49:03 -0500

    user now pulled from investigator if logged in

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,9 @@ class MessagesController < ApplicationController
 
   def create
     new_message = Message.new(message_params)
-    new_message.messageable = return_user
-    binding.pry
+    new_message.messageable = return_user(new_message)
+    new_message.save
+    redirect_to :back
   end
 
   private
@@ -13,8 +14,9 @@ class MessagesController < ApplicationController
       params.require(:message).permit(:complaint_id, :text)
     end
 
-    def return_user
-      User.first
+    def return_user(new_message)
+      creating_user = new_message.complaint.user
+      current_investigator ? current_investigator : creating_user
     end
 
 end
