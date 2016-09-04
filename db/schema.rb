a30cd081b14022c6c5d07d0d5a4d60e7ecc4b472
Joseph Mazrimas <email": null>
2016-09-04 14:36:32 -0500

    and adding db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903151609) do
+ActiveRecord::Schema.define(version: 20160904172848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,15 @@ ActiveRecord::Schema.define(version: 20160903151609) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["complaint_id"], name: "index_media_on_complaint_id", using: :btree
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "complaint_id"
+    t.integer  "messageable_id"
+    t.string   "messageable_type"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "users", force: :cascade do |t|
