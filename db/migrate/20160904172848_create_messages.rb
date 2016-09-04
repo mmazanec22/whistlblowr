ff4ffaac6f670b0a29c5af5d295f9fe03f555169
Joseph Mazrimas <email": null>
2016-09-04 14:35:53 -0500

    adding db changes for message migration

--- db/migrate/20160904172848_create_messages.rb
+++ db/migrate/20160904172848_create_messages.rb
@@ -2,6 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.text    :text
+      t.integer :complaint_id
       t.integer :messageable_id
       t.string  :messageable_type
       t.timestamps
