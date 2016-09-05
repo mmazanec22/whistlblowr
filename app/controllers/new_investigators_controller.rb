7fe8c4284d049c181c92930663387830e85aab79
Alex S <email": null>
2016-09-05 18:15:09 -0500

    travis updates

--- app/controllers/new_investigators_controller.rb
+++ app/controllers/new_investigators_controller.rb
@@ -6,19 +6,15 @@ class NewInvestigatorsController < ApplicationController
   end
 
   def create
-    if current_investigator.admin?
-      @email = params[:email]
-      @investigator = Investigator.new(email: @email, password: "secretsix")
-      if @investigator.save
-        @errors = ["user saved"]
-        render 'new_investigators/new'
-      else
-        @errors = ["user NOT saved"]
-        render 'new_investigators/new'
-      end
+
+    email = params[:email]
+    @investigator = Investigator.new(email: email, password: "secretsix")
+    if @investigator.save
+      @errors = ["user saved"]
+      render 'new_investigators/new'
     else
-      @errors = ["you are not authorized"]
-      render "new"
+      @errors = ["user NOT saved"]
+      render 'new_investigators/new'
     end
   end
 
