ff6d12e4d7c3888e56dff97bb59504f7d3e3133a
Ben Van Sickle <email": null>
2016-09-05 15:08:00 -0500

    set back up for web stuffff

--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -9,8 +9,8 @@ class MediaUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
 
   # Override the directory where uploaded files will be stored.
