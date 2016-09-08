fa7c04eb37b1fac3a512a7c84ec2915c68a638fe
Ben Van Sickle <email": null>
2016-09-08 14:58:18 -0500

    404 finally working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
@@ -7,6 +8,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     complaints_path
+  end
+
+  def not_found
+    raise ActionController::RoutingError.new("Nothing fucking found")
   end
 
   # check_authorization :unless => :devise_controller?  # This will raise an exception if authorization is not performed in an action.
--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,10 @@ Bundler.require(*Rails.groups)
 
 module Whistlblowr
   class Application < Rails::Application
+    config.exceptions_app = self.routes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+/home/ben/.rbenv/versions/2.3.1/lib/ruby/gems/2.3.0/gems/actionpack-5.0.0.1/lib/action_dispatch/middleware/templates/rescues/_trace.html.erb
