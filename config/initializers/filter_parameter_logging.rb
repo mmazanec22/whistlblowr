7ce84c06444289cb7833d8b9d07cb2244a8fe275
mmazanec22 <melanie.mazanec@gmail.com>
2016-09-17 14:12:44 -0500

    Filter using regex maybe

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :fwd]
+Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += /^((?:(?:^|\.)(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])){4})$/
