d0b1e9930fe53b74d47bbbb8c6aa43a86ef406e8
jmazrimas <joe.mazrimas@gmail.com>
2016-09-19 18:22:02 -0500

    show route now checks PIN in params

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -53,11 +53,10 @@ class ComplaintsController < ApplicationController
     @investigator_authenticated = false
     @investigator_authenticated = true if current_investigator
     @message = Message.new
-    @complaint = @complaint ? @complaint : Complaint.find_by(key: params[:complaint_key])
-    if @complaint == nil
+
+    @complaint = @complaint ? @complaint : Complaint.find_by(key: params[:complaint_key], pin: params[:complaint_pin])
+    if !@complaint
       redirect_to custom_errors_no_match_path
-    # elsif @complaint.pin != params[:complaint_pin]
-    #   redirect_to custom_errors_no_match_path
     else
       @messages = @complaint.messages.order("created_at DESC").page(params[:page]).per(10)
       @complaint.messages.each {|m| m.update_attribute(:viewed, true)}
