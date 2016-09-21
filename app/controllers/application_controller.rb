59b677b6b8b35daeb58bb0fe7a1bcdd4e507f93c
Ben Van Sickle <email": null>
2016-09-21 10:52:30 -0500

    commented out cancan, need new email pw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
+  # rescue_from CanCan::AccessDenied do |exception|
+  #   redirect_to root_url, :alert => exception.message
+  # end
 
   def after_sign_in_path_for(resource)
     complaints_path
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
 
   # check_authorization :unless => :devise_controller?  # This will raise an exception if authorization is not performed in an action.
 
-  skip_authorization_check  # To skip check_authorization
+  # skip_authorization_check  # To skip check_authorization
 
 end
